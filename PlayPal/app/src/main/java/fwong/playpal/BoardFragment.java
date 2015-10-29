496321ff9923ff070143c4002651514bfdf28484
FrederickWong <frederickwong94@gmail.com>
2015-10-29 21:26:27 +0800

    PlayPal Tablet Updated

---
+++ PlayPal/app/src/main/java/fwong/playpal/BoardFragment.java
@@ -0,0 +1,50 @@
+package fwong.playpal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class BoardFragment extends Fragment {
+
+    public BoardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_board, container, false);
+        Button playertab = (Button) rootView.findViewById(R.id.playertab);
+        playertab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Coming Soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ PlayPal/app/src/main/java/fwong/playpal/DrawerAdapter.java
@@ -0,0 +1,58 @@
+package fwong.playpal;
+
+        import android.content.Context;
+        import android.view.LayoutInflater;
+        import android.support.v7.widget.RecyclerView;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import java.util.Collections;
+        import java.util.List;
+
+        import fwong.playpal.R;
+        import fwong.playpal.NavDrawerItem;
+
+public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.MyViewHolder> {
+    List<NavDrawerItem> data = Collections.emptyList();
+    private LayoutInflater inflater;
+    private Context context;
+
+    public DrawerAdapter(Context context, List<NavDrawerItem> data) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
+    public void delete(int position) {
+        data.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.nav_drawer_row, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        NavDrawerItem current = data.get(position);
+        holder.title.setText(current.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+}
